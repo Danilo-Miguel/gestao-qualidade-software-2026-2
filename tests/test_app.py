12b490b27db526a3fd01d0fd5423504091720af8
Danilo-Miguel <danilo.miguel@oceanbrasil.com>
2026-09-01 19:45:38 -0300

    Criação do arquivo de testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,5 +7,11 @@ from app import app
 def test_health_check():
     client = app.test_client()
     response = client.get('/health')
-    assert response.status.code == 200
+    assert response.status_code == 200
     assert response.get_json()["status"] == "ok"
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')    
+    assert response.status_code == 200
